3683d9e8dc271eca887aa8bb71c0cb7e3412a791
mike <mikewknowles@gmail.com>
2014-05-21 15:35:33 -0400

    Changed threading method

--- ARMI.py
+++ ARMI.py
@@ -1,4 +1,3 @@
-__author__ = 'blais'
 __author__ = 'mikeknowles'
 """ Includes threading found in examples:
 http://www.troyfawkes.com/learn-python-multithreading-queues-basics/
@@ -7,22 +6,19 @@ https://docs.python.org/2/library/threading.html
 """
 from Bio.Blast.Applications import NcbiblastnCommandline
 from Bio import SeqIO
-import subprocess
-import os
-import glob
-import time
-import sys
 from threading import Thread
 from Queue import Queue
-import shlex
-import argparse
+import subprocess, os, glob, time, sys, shlex, argparse, re
+
 
 def makeblastdb(queue):
     while True:
         #grabs fastapath from queue
         fastapath = queue.get()
         #makeblastdb if not exist
-        if not os.path.isfile(fastapath + ".nhr"):
+        nhr = "%s.nhr" % (fastapath)
+        if not os.path.isfile(str(nhr)):
+            print "makeblastdb -in %s -dbtype nucl -out %s" % (fastapath, fastapath)
             subprocess.Popen(shlex.split("makeblastdb -in %s -dbtype nucl -out %s" % (fastapath, fastapath)),
                              stdout=open(os.devnull, 'wb'))
             sys.stdout.write('.')
@@ -32,6 +28,7 @@ def makeblastdb(queue):
         queue.task_done()
 
 queue = Queue()
+queue2 = Queue()
 
 def makedbthreads(fastas):
     ''' Setup and create threads for class'''
@@ -44,11 +41,39 @@ def makedbthreads(fastas):
     #wait on the queue until everything has been processed
     queue.join()
 
-def blaster(path, out):
+def runblast(queue2):
+    while True:
+        genomefasta = queue2.get()
+        for key in genomefasta:
+            for db in key:
+                print db
+                gene = re.search('\/(\w+)\.fasta', db)
+                blastn = NcbiblastnCommandline(query=key, db=db, evalue=1e-40, out=gene.group(1))
+                print blastn
+                subprocess.Popen(shlex.split(blastn))
+        queue2.task_done()
+
+def blastnthreads(fastas, genomes):
+    for i in range(len(fastas) * len(genomes)):
+        threads = Thread(target=runblast, args=(queue2,))
+        threads.setDaemon(True)
+        threads.start()
+    genomefasta = {}
+    for genome in genomes:
+        genomefasta.setdefault(genome, []).append(fastas)
+        queue2.put(genomefasta)
+    #wait on the queue until everything has been processed
+    queue2.join()
+
+def blaster(path, targets, out):
     fastas = glob.glob(path + "*.fasta")
+    genomes = glob.glob(targets + "*.fasta")
     sys.stdout.write("[%s] Creating necessary databases for BLAST" % (time.strftime("%H:%M:%S")))
     makedbthreads(fastas)
     print "\n[%s] BLAST database(s) created" % (time.strftime("%H:%M:%S"))
+    print "[%s] Now performing BLAST database searches" % (time.strftime("%H:%M:%S"))
+    blastnthreads(fastas, genomes)
+
 
 
 
@@ -59,4 +84,4 @@ parser.add_argument('-i', '--input', required=True, help='Specify input fasta fo
 parser.add_argument('-m', '--marker', required=True, help='Specify antibiotic markers folder')
 parser.add_argument('-o', '--output', required=True, help='Specify output folder for csv')
 args = vars(parser.parse_args())
-blaster(args['input'], args['output'])
+blaster(args['marker'], args['input'], args['output'])
